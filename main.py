0ce8ca8c87e693824e6b530f60f14a470d416b11
TaeWoo Nam <ntw8651@gmail.com>
2024-11-08 17:46:25 +0900

    초기 프로젝트 세팅 - 1
    진행 중,

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
+from data_process import * #데이터 처리 코드들 가져오기
+from crawling import * #크롤링 코드들 가져오기
+
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5 import uic
 
+
+
+
+################### 이곳에 pyqt 디자이너로 만든 ui 파일 경로를 적기
+#main.py와 같은 폴더에 넣고 파일명만 적으면 됨
 form_class = uic.loadUiType("untitled.ui")[0]
+###################
+
+
+###################
+# 이곳에 각 위젯 작성
+###################
 
 class WindowClass(QMainWindow, form_class) :
     def __init__(self) :
@@ -16,6 +30,13 @@ class WindowClass(QMainWindow, form_class) :
 
 
 
+
+
+
+
+
+
+# main 실행은 맨 마지막에 두기
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
     myWindow = WindowClass()
